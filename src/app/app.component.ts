4a2ec881ef51374a2cb8a9486105c180488a93f9
Safak <safak.inan61@hotmail.com>
2021-06-18 20:02:27 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements AfterViewInit {
+
+
+  @ViewChild('welcomeAnimation') 
+  private animationEl: ElementRef;
+  animationFinished: boolean;
+  
+  constructor() {}
+  
+  ngAfterViewInit() {
+    this.animationEl.nativeElement.addEventListener('animationend', () => {
+      this.animationFinished = true;
+    });
+  }
+
+  
 }
