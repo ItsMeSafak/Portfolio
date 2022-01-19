324812465397606c5d00bfba9bd16ebce2216805
Safak Inan <safak.inan61@hotmail.com>
2022-01-19 23:47:11 +0100

    updated everything

--- src/app/partials/header/header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'partial-header',
-	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.scss'],
-})
-export class HeaderComponent {}
